b7bb79fe07ca28f3e4557951b1ddfa6bdd3fda1d
Hugo Vetrano <hugo.vetrano@ieseg.fr>
2023-06-14 15:35:45 +0200

    m

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,9 +7,25 @@ class RestaurantsController < ApplicationController
   def show
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render :new
+    end
+  end
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone_number, :category, :address)
   end
 end
